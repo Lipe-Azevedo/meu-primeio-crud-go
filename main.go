35d5c461485ff35f2b021070d9faa24ae785ade1
Lipe-Azevedo <luizfelipeazevedobrito1@gmail.com>
2025-04-26 21:52:23 -0300

    commit

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
+// package main
+
+// import (
+// 	"log"
+// 	"os"
+
+// 	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/controller/routes"
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/joho/godotenv"
+// )
+
+// func main() {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		port = "8080"
+// 	}
+
+// 	router := gin.Default()
+// 	routes.InitRoutes(&router.RouterGroup)
+
+// 	log.Println("Servidor rodando na porta " + port)
+// 	router.Run(":" + port)
+// }
+
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/controller/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -14,5 +43,11 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	fmt.Println(os.Getenv("TEST"))
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,24 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/configuration/rest_err"
+	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+}
