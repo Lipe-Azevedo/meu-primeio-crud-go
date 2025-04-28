d06e8da485844f7967c249846efd14ba3ab1c428
Lipe-Azevedo <luizfelipeazevedobrito1@gmail.com>
2025-04-28 20:54:13 -0300

    commit - 6

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,14 +1,19 @@
 package controller
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/configuration/logger"
 	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/configuration/validation"
 	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/controller/model/request"
-	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/controller/model/response"
+	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/model"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+var (
+	UserDomainInterface model.UserDomainInterface
 )
 
 func CreateUser(c *gin.Context) {
@@ -23,13 +28,20 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
-	fmt.Println(userRequest)
-	response := response.UserResponse{
-		ID:    "test",
-		Email: userRequest.Email,
-		Name:  userRequest.Name,
-		Age:   userRequest.Age,
+	domain := model.NewUserDomain(
+		userRequest.Email,
+		userRequest.Password,
+		userRequest.Name,
+		userRequest.Age,
+	)
+	if err := domain.CreateUser(); err != nil {
+		c.JSON(err.Code, err)
+		return
+
 	}
 
-	c.JSON(http.StatusOK, response)
+	logger.Info("User created sucessfully",
+		zap.String("journey", "createUser"))
+
+	c.String(http.StatusOK, "")
 }
---
+++ src/model/create_user.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"fmt"
+
+	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/configuration/logger"
+	"github.com/Lipe-Azevedo/meu-primeio-crud-go/src/configuration/rest_err"
+	"go.uber.org/zap"
+)
+
+func (ud *UserDomain) CreateUser() *rest_err.RestErr {
+
+	logger.Info("Init createUser model", zap.String("journey", "createUser"))
+	ud.EncryptPassword()
+
+	fmt.Println(ud)
+
+	return nil
+}
